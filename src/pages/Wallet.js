b84f54e06df0f5d4f9cf665406aa90d68eb07b63
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-06-08 20:25:53 -0300

    Desafio 5 - [DONE]

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    const { saveEmail } = this.props;
+    return (
+      <div>
+        <header>
+          <div data-testid="email-field">
+            {saveEmail}
+          </div>
+          <div data-testid="total-field">
+            0
+          </div>
+          <div data-testid="header-currency-field">
+            - Um elemento que exiba o email do usuário que fez login.
+            - Crie um campo com a despesa total gerada pela lista de gastos.
+            - Crie um campo que mostre que qual câmbio está sendo utilizado,
+            que será neste caso BRL
+          </div>
+        </header>
+      </div>
+    );
   }
 }
 
-export default Wallet;
+const mapStateToProps = (state) => ({
+  saveEmail: state.user.email,
+});
+
+Wallet.propTypes = {
+  saveEmail: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps, null)(Wallet);
