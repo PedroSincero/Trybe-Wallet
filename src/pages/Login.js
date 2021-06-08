a3bcfda8c24e4ab8dc156e7effb444171fb3768c
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-06-08 15:24:02 -0300

    Desafio 1 - [DONE]

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <form>
+        <h1>Login</h1>
+        <label htmlFor="email">
+          Email:
+          <input id="email" type="email" data-testid="email-input" />
+        </label>
+        <label htmlFor="password">
+          Senha:
+          <input id="password" type="password" data-testid="password-input" />
+        </label>
+        <Link to="/">
+          <button type="button"> Entrar</button>
+        </Link>
+      </form>
+    );
   }
 }
 
