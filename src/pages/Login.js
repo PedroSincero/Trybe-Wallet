952993a8382999b4fdfac3e6d1aa65dc14450228
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-06-08 18:28:04 -0300

    adicionando correçoes do eslint

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,62 +6,79 @@ class Login extends React.Component {
     super();
 
     this.state = {
-      disabled: true,
       email: '',
       password: '',
       validEmail: false,
       validPassword: false,
-    }
-
+    };
+    this.handleValidEmail = this.handleValidEmail.bind(this);
+    this.handleValidPassword = this.handleValidPassword(this);
   }
 
-  handleValidEmail = (event) => {
-    const { target: { value, id }} = event;
+  handleValidEmail(event) {
+    const { target: { value, id } } = event;
     const { email } = this.state;
-    const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/ 
+    const regex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
 
     this.setState({
-    [id]: value,
-    validEmail: regex.test(email),
+      [id]: value,
+      validEmail: regex.test(email),
     });
   }
 
-  handleValidPassword = (event) => {
-    const { target: { value, id }} = event;
-    const { password } = this.state
-    const NUM_MAGIC = 5
+  handleValidPassword(event) {
+    const { target: { value, id } } = event;
+    const { password } = this.state;
+    const NUM_MAGIC = 5;
 
     this.setState({
-    [id]: value,
-    validPassword: password.length >= NUM_MAGIC,
-    })
+      [id]: value,
+      validPassword: password.length >= NUM_MAGIC,
+    });
   }
 
   render() {
-    const { email, password, validPassword, validEmail } = this.state
+    const { email, password, validPassword, validEmail } = this.state;
     return (
       <form>
         <h1>Login</h1>
         <label htmlFor="email">
           Email:
-          <input id="email" type="email" value={email} onChange={this.handleValidEmail} data-testid="email-input" />
+          <input
+            id="email"
+            type="email"
+            value={ email }
+            onChange={ this.handleValidEmail }
+            data-testid="email-input"
+          />
         </label>
         <label htmlFor="password">
           Senha:
-          <input id="password" type="password" value={password} onChange={this.handleValidPassword}data-testid="password-input" />
+          <input
+            id="password"
+            type="password"
+            value={ password }
+            onChange={ this.handleValidPassword }
+            data-testid="password-input"
+          />
         </label>
         <Link to="/carteira">
-          <button type="button" disabled={ !validEmail || !validPassword  } > Entrar</button>
-          
+          <button
+            type="button"
+            disabled={ !validEmail || !validPassword }
+          >
+            Entrar
+          </button>
+
         </Link>
       </form>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
+// const mapDispatchToProps = (dispatch) => ({
 
-})
+// });
 export default Login;
 
 // Agradecimentos A Alessandra Razende  Turma 10 Tribo b - e ao Instrutor Eduardo Santos
